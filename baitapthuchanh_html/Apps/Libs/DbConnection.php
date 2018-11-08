967a29e98c83af8dde8465cc5211386a7ceb00a6
Thanh Van <ntvan1504@gmail.com>
2018-11-08 15:52:50 +0700

    doi ten database

--- baitapthuchanh_html/Apps/Libs/DbConnection.php
+++ baitapthuchanh_html/Apps/Libs/DbConnection.php
@@ -7,7 +7,7 @@ class Apps_Libs_DbConnection{
     protected $host = 'localhost';
     protected $userName = 'root';
     protected $passWord = '';
-    protected $database = 'baitapthuchanh';
+    protected $database = 'baitapthuchanh2';
     
     protected $queryParams = []; //biến lưu trữ các params phục vụ cho query
 
